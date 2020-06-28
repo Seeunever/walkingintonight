055af049f288806c167d0fb009a63a7a1cf33df8
Seeunever <irene11wang@outlook.com>
2020-06-28 16:36:42 +0800

    20200628-2

--- 备份/blood/static/js/character.js
+++ 备份/blood/static/js/character.js
@@ -1,12 +1,29 @@
-// var job = document.getElementById('job');
-
-// function save(){
-//     var job_str = job.value;
-//     localStorage.setItem('job',job_str);
-// }
-
-// function load(){
-//     var job_str = job.value;
-//     job.innerHTML = localStorage.getItem('job')
-// }
+/* 鼠标特效 */
+var a_idx = 0;
+jQuery(document).ready(function ($) {
+    $("body").click(function (e) {
+        var a = new Array("❤");
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+            y = e.pageY;
+        $i.css({
+            "z-index": 99,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb(" + ~~(255 * Math.random()) + "," + 0 + "," + 0 + ")"
+        });
+        $("body").append($i);
+        $i.animate({
+                "top": y - 180,
+                "opacity": 0
+            },
+            1500,
+            function () {
+                $i.remove();
+            });
+    })
+});
 
